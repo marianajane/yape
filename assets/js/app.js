9b79d620d7fcf0fe30a9e45309c1c90d9fb80b88
marianajane <mariana.musm@gmail.com>
2017-07-19 12:49:32 -0500

    desarrollo validaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
+
+//Carousel
  $(document).ready(function(){
       $('.carousel.carousel-slider').carousel();
     });
+
+/*Validación números
+function continuar registro() {
+    var form = document.getElementById("phone_number");
+    var phone = "";
+    for (var i = 0; i < form.length ;i++) {
+       
+    }
+}
+if numero == checked && validate=checked
+ */
